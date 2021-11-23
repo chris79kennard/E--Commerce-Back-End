a94105028ab7d3799585c7edd221cdce7e1c0bf4
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 17:08:42 -0600

    created post route for creating a new tag.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,14 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagPostData = await Tag.create(req.body);
+    res.status(200).json(tagPostData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
