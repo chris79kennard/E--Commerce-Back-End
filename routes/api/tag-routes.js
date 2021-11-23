0fc2270ec88aa2f7bd50ae6497875af541b4ca17
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 17:07:45 -0600

    created get by id function for getting a single tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,8 +16,21 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const taggedProduct = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    console.log(taggedProduct);
+    if (!taggedProduct) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(taggedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
