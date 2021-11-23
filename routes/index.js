771b2f8302e9d57205f3f4fe18e42a48647417ff
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 17:23:52 -0600

    renamed .env file

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Come on Chris! You can figure this out man! </h1>")
 });
 
 module.exports = router;
