e0273c99621ab9514b99a6c89ccd5c926ab06d92
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 16:57:05 -0600

    created put id function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryPutData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      }, 
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!categoryPutData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryPutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
