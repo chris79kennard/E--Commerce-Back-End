9da14b93f6953b33f0d871335beabc9090879323
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 16:54:52 -0600

    added get by id function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,8 +16,21 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const taggedCategory = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    console.log(taggedCategory);
+    if (!taggedCategory) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(taggedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   
   // be sure to include its associated Products
 });
