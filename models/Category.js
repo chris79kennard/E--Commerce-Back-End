5be99f99dc0e51979b97f8b79159edea677677cb
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 17:34:26 -0600

    defined colums for product tag class

--- models/Category.js
+++ models/Category.js
@@ -2,19 +2,39 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class ProductTag extends Model {}
 
-Category.init(
+ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: 'product_tag',
   }
 );
 
-module.exports = Category;
+module.exports = ProductTag;
