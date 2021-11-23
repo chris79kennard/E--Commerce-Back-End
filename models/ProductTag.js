d0502b86d6305a6a97c0ffc27560a0ecd69da99d
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 17:56:20 -0600

    new error popped up stating Unhandled rejection SequelizeAccessDeniedError: Access denied for user ''@'localhost' (using password: YES)

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,13 +7,22 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'tag',
   }
 );
 
