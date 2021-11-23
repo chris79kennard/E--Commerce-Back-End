207944c0fc897f13b4ae2f589b0c5bb46a0ae26a
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-11-23 16:53:31 -0600

    corrected router get function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,11 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
-});
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
   try {
     const categoryData = await Category.findAll({
       include: [{model: Product}]
@@ -18,6 +13,12 @@ router.get('/:id', (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+  // be sure to include its associated Products
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  
   // be sure to include its associated Products
 });
 
